fb8496505c48a6651b592011bc198257a16a4fcc
Kelsonroberto <robertokelsonn@gmail.com>
2023-04-09 19:40:28 -0300

    Primeira versão do portifólio

---
+++ script.js
@@ -0,0 +1,12 @@
+
+// let animationImg = document.querySelector('.minhaFoto')
+// animation.addEventListener('animationend', () => {    
+// })
+
+
+function animationSection(){
+let section = document.getElementsByClassName('emprego').textContent
+ 
+console.log(section)
+//   animationSection.textContent = animationSection
+}
